6b32c1f462f450fa41b4fd9300491a8f8b10b04d
Cy-xin <1257425345@qq.com>
2019-07-02 14:01:40 +0800

    update login

--- src/main/java/com/community/controller/AuthorizeController.java
+++ src/main/java/com/community/controller/AuthorizeController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -16,17 +18,24 @@ public class AuthorizeController {
 
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id("4a42dde0f32d931edfb2");
         accessTokenDTO.setClient_secret("2d90b95732250bb69259477dbf467b735314673d");
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("https://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = gitHubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null){
+            //登录成功  写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "index";
+        }else {
+            //登录失败
+            return "index";
+        }
     }
 
 }
